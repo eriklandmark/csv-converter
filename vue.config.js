7a08e3c33188a1f8b33d98978b0b95fcf08e4013
erikl <erik@exeri.se>
2022-10-07 13:01:07 +0200

    Updated dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,24 @@
-module.exports = {
-    publicPath: '',
+const {defineConfig} = require('@vue/cli-service')
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = defineConfig({
+    transpileDependencies: [
+        'vuetify'
+    ],
     pluginOptions: {
         electronBuilder: {
+            outputDir: 'electron',
             nodeIntegration: true,
+            externals: [],
+            customFileProtocol: './'
         }
     },
-}
+    configureWebpack: {
+        plugins: [new NodePolyfillPlugin()],
+        optimization: {
+            splitChunks: {
+                chunks: "all",
+            },
+        },
+    },
+})
